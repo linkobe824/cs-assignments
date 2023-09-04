244c7160969111d3bc5679d237a6a07fe6371220
Manuel Beltran <136205896+linkobe824@users.noreply.github.com>
2023-09-03 18:34:25 -0700

    fibonacci recursive solution

--- fibs.js
+++ fibs.js
@@ -1,19 +1,25 @@
 function fibs(n) {
-    const res = []
-    for(let i = 0; i < n; i++) {
-        if(i <= 1){
-            res.push(i)
-        }else {
-            res.push(res[i-2] + res[i - 1])
-        }
+  const res = [];
+  for (let i = 0; i < n; i++) {
+    if (i <= 1) {
+      res.push(i);
+    } else {
+      res.push(res[i - 2] + res[i - 1]);
     }
+  }
 
-    return res
+  return res;
 }
 
 function fibsRec(n) {
-    
+  if (n === 2) {
+    return [0, 1];
+  } else {
+    return fibsRec(n - 1).concat(
+      fibsRec(n - 1)[n - 2 - 1] + fibsRec(n - 1)[n - 1 - 1]
+    );
+  }
 }
 
-console.log(fibs(8))
-console.log(fibsRec(8))
+console.log(fibs(8));
+console.log(fibsRec(8));
