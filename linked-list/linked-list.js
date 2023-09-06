b06d33fd815e31e03cbff770b1468a92972132c3
Manuel Beltran <136205896+linkobe824@users.noreply.github.com>
2023-09-05 20:53:16 -0700

    first commit - node and linkedlist factory function with append method

---
+++ linked-list/linked-list.js
@@ -0,0 +1,35 @@
+function Node(data) {
+  return {
+    data,
+    next: null,
+  };
+}
+
+function LinkedList() {
+  // Adds a new value at the end of the list
+  function append(value) {
+    const newNode = Node(value);
+    if (this.head == null) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      let cur = this.head;
+      while (cur.next != null) {
+        cur = cur.next;
+      }
+      cur.next = newNode;
+      this.tail = newNode;
+    }
+    console.log(this);
+  }
+
+  return {
+    head: null,
+    tail: null,
+    append,
+  };
+}
+
+const ll = LinkedList();
+ll.append(5);
+ll.append(7);
