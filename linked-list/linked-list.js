5915a19b384b2d65db3d0e0c5cc6f5b3496a4d98
Manuel Beltran <136205896+linkobe824@users.noreply.github.com>
2023-09-06 09:36:42 -0700

    add prepend method

--- linked-list/linked-list.js
+++ linked-list/linked-list.js
@@ -3,6 +3,8 @@ ll.append(5);
 ll.append(7);
 ll.append(3);
 
+ll.prepend(1);
+
 function Node(data) {
   return {
     data,
@@ -26,7 +28,7 @@ function LinkedList() {
   //     this.tail = newNode;
   //   }
   //   console.log(this);
-  
+
   // Adds a new value at the end of the list
   function append(value) {
     const newNode = Node(value);
@@ -40,9 +42,24 @@ function LinkedList() {
     console.log(this);
   }
 
+  // Adds a new value at the start of the list
+  function prepend(value){
+    const newNode = Node(value);
+    if (this.head === null) {
+      this.head = newNode;
+      this.tail = newNode;
+    }
+    else {
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+    console.log(this);
+  }
+
   return {
     head: null,
     tail: null,
     append,
+    prepend,
   };
 }
